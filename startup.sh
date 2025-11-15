eb385ce79c93a8f53c4635391949bde64b9af27c
Hayotunday <idowudanielayotunde@gmail.com>
2025-11-15 12:37:25 +0100

    removed the start command from startup.sh

--- startup.sh
+++ startup.sh
@@ -9,6 +9,3 @@ pip install --no-cache-dir -r requirements.txt
 
 echo "Downloading latest dataset..."
 python scripts/update_dataset.py
-
-echo "Starting API..."
-exec uvicorn src.main:app --host 0.0.0.0 --port $PORT
